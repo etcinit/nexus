5801a04e1ca0e766d3ee86f5e8aad763365ddb17
Eduardo Trujillo <ed@chromabits.com>
2014-08-07 02:13:00 -0400

    Tons of design chnages

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            dist: {
+                src: [
+                    'browser/*.js',
+                    'browser/*/**.js'
+                ],
+                dest: 'public/js/app.js'
+            }
+        },
+
+        sass: {
+            options: {
+                includePaths: ['public/components/foundation/scss']
+            },
+
+            dist: {
+                options: {
+                    outputStyle: 'compressed'
+                },
+
+                files: {
+                    'public/css/app.css': 'scss/app.scss'
+                }
+            }
+        },
+
+        autoprefixer: {
+            options: {
+
+            },
+
+            dist: {
+                src: 'public/css/app.css',
+                dest: 'public/css/app.css'
+            }
+        },
+
+        watch: {
+            sass: {
+                files: 'scss/**/*.scss',
+                tasks: ['sass', 'autoprefixer']
+            },
+
+            js: {
+                files: 'browser/**/*.js',
+                tasks: ['concat']
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+
+    grunt.registerTask('default', ['sass', 'autoprefixer', 'concat', 'watch']);
+};
