603ebd3d5bce8b931935bcc0699231864163db06
Eduardo Trujillo <ed@chromabits.com>
2015-03-02 11:31:39 -0500

    Use new version of Enclosure

--- app.js
+++ app.js
@@ -1,40 +1,5 @@
-"use strict";
+'use strict';
 
 require('ensure.js');
 require("babel/register"); // Use ES6
-require('enclosure').bootstrap();
-
-var path = require('path');
-
-var Application = use('Chromabits/Container/Application'),
-    Loader = use('Chromabits/Loader/Loader'),
-    DirectoryMapper = use('Chromabits/Mapper/DirectoryMapper'),
-    EnclosureMap = use('Chromabits/Mapper/EnclosureClassMap');
-
-// Setup class autoloading
-var loader = new Loader();
-var mapper = new DirectoryMapper(path.resolve(__dirname, './src/'));
-
-loader.addMap(mapper.generate());
-loader.addMap(EnclosureMap);
-
-// Start the service container
-var application = new Application();
-
-application.setLoader(loader);
-application.installTo(global);
-
-// Register providers
-container.addProvider('Providers/ConfigServiceProvider');
-container.addProvider('Providers/PreludeServiceProvider');
-container.addProvider('Providers/ServerServiceProvider');
-
-container.register();
-container.bootProviders();
-
-var prelude = container.make('Prelude');
-
-// Instantiate a new Nexus server and start listening
-if (prelude.shouldLoadServer()) {
-    container.make('NexusServer').listen();
-}
+require('enclosure').boot();
