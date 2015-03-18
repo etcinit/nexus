671b905084bbfcc1e8e8bacaec4946268994fabd
Eduardo Trujillo <ed@chromabits.com>
2015-03-18 16:56:09 -0400

    Increase file size limit

--- src/NexusServer.js
+++ src/NexusServer.js
@@ -42,8 +42,8 @@ class NexusServer
         this.setupLog();
 
         // Setup body parsers
-        app.use(bodyParser.urlencoded({ extended: false }));
-        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+        app.use(bodyParser.json({ limit: '50mb' }));
 
         // Setup session
         app.use(session({
